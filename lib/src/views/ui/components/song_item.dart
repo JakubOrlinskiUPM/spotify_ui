e146e909c3804ad6df3b110004451b0142773f30
jakuborlinski <jakub.orlinski.upm@gmail.com>
2021-11-23 01:40:21 +0100

    Song listen to the player bloc

--- lib/src/views/ui/components/song_item.dart
+++ lib/src/views/ui/components/song_item.dart
@@ -2,39 +2,67 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:spotify_ui/src/business_logic/blocs/data_bloc.dart';
 import 'package:spotify_ui/src/business_logic/models/playlist.dart';
 import 'package:spotify_ui/src/business_logic/models/song.dart';
 import 'package:spotify_ui/src/business_logic/blocs/player_bloc.dart';
+import 'package:spotify_ui/src/views/ui/components/menu_dialog.dart';
 
 class SongItem extends StatelessWidget {
-  const SongItem({Key? key, required this.song, required this.playlist}) : super(key: key);
+  const SongItem({Key? key, required this.song, required this.playlist})
+      : super(key: key);
 
   final Playlist playlist;
   final Song song;
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => _onSongPressed(context),
-      child: ListTile(
-        contentPadding: EdgeInsets.only(left: 8.0),
-        leading: SizedBox(
-          width: 50,
-          height: 50,
-          child: CachedNetworkImage(
-            imageUrl: song.album.coverUrl,
+    return BlocBuilder<PlayerBloc, PlayerState>(
+      builder: (context, state) {
+        TextStyle style = TextStyle(
+          color: state.song?.id == song.id ? Colors.green : null,
+        );
+
+        return TextButton(
+          onPressed: () => _onSongPressed(context),
+          child: ListTile(
+            contentPadding: EdgeInsets.only(left: 8.0),
+            leading: SizedBox(
+              width: 50,
+              height: 50,
+              child: CachedNetworkImage(
+                imageUrl: song.album.coverUrl,
+              ),
+            ),
+            title: Text(
+              song.title,
+              style: style,
+            ),
+            subtitle: Text(song.authorString),
+            trailing: IconButton(
+              onPressed: () => _onMenuPressed(context),
+              icon: const Icon(Icons.more_vert),
+            ),
           ),
-        ),
-        title: Text(song.title),
-        subtitle: Text(song.authorString),
-        trailing: Text("hi!"),
-      ),
+        );
+      },
     );
   }
 
   void _onSongPressed(BuildContext context) {
-    context.read<PlayerBloc>().add(PlayerSetSongEvent(song: song, playlist: playlist));
+    context
+        .read<PlayerBloc>()
+        .add(PlayerSetSongEvent(song: song, playlist: playlist));
     context.read<PlayerBloc>().add(PlayerStartedEvent());
+  }
+
+  _onMenuPressed(BuildContext context) {
+    showModalBottomSheet<void>(
+      useRootNavigator: true,
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext context) {
+        return MenuDialog();
+      },
+    );
   }
 }
